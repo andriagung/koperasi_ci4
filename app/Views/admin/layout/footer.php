65e63769579c589ddbe651ab5518a549d8c34162
Agung Andri <Andriagung7@gmail.com>
2026-09-05 04:22:09 +0700

    fix(views): add cache busting to admin.js to ensure client loads latest changes

--- app/Views/admin/layout/footer.php
+++ app/Views/admin/layout/footer.php
@@ -354,6 +354,6 @@
             }
         };
     </script>
-    <script src="<?= base_url('assets/js/admin.js') ?>"></script>
+    <script src="<?= base_url('assets/js/admin.js?v=' . time()) ?>"></script>
 </body>
 </html>
